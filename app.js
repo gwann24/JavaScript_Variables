517fb11a5b0c3b9d07f890759c733cb1ef869894
Greg Wanner <greg@tegogroup.com>
2024-03-26 14:50:16 -0400

    Completed JavaScript variable exercise

--- app.js
+++ app.js
@@ -5,6 +5,55 @@ console.log(
 
 // Exercise 1
 
+let lastname;
+var age;
+
 // Exercise 2
 
+const firstname = "Greg";
+lastname = "Wanner";
+age = 55;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+
 // Exercise 3
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+
+//Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
